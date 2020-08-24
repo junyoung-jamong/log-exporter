87ae2cf94d06954e33b838aa3bbed446ebcc8d7f
Kang Junyoung <jykang@coinplug.com>
2020-08-24 12:08:49 +0900

    시작일자 Query 오류 해결 및 제외 문자열 추가

--- log_exporter.go
+++ log_exporter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os/exec"
 	"strconv"
@@ -22,6 +23,10 @@ const LOG_DIRECTORY = "/opt/meta/logs/"
 const LAYOUT = "2006-01-02T15:04:05.000Z"
 
 var FILE_LIST = [6]string{"log", "log.1", "log.2", "log.3", "log.4", "log.5"}
+
+var EXCLUDE_LIST = [1]string{
+	"Stats server unreachable",
+}
 
 func main() {
 	r := gin.Default()
@@ -76,12 +81,18 @@ func ReStart(c *gin.Context) {
 
 func GetLogs(c *gin.Context) {
 	start := c.DefaultQuery("start", "")
-	startTime, err := time.Parse(LAYOUT, start)
+	fmt.Println("start: ", start)
+
+	startInt, err := strconv.ParseInt(start, 10, 64)
 
 	hasRange := true
+	startTime := time.Unix(0, 0) //Default Time: 1970-01-01 09:00:00 +0900 KST
 
 	if err != nil {
 		hasRange = false
+	} else {
+		startTime = time.Unix(startInt, 0)
+		fmt.Println("startTime", startTime)
 	}
 
 	currentDate := time.Now().Format("2006-01-02")
@@ -94,7 +105,7 @@ func GetLogs(c *gin.Context) {
 		filePath := LOG_DIRECTORY + fileName
 
 		f, err := ioutil.ReadFile(filePath)
-		if err != nil {
+		if err != nil { //파일이 존재하지 않거나 접근에 실패했을 경우
 			continue
 		}
 
@@ -102,7 +113,7 @@ func GetLogs(c *gin.Context) {
 		page = segment + page
 
 		if hasRange {
-			pageStartDt := segment[6:11] + "T" + segment[12:24] + "Z"
+			pageStartDt := segment[6:11] + "T" + segment[12:24] + "Z" //Log파일의 첫 번째 Log 일자
 			month, _ := strconv.ParseInt(segment[6:8], 10, 64)
 
 			if month > currentMonth {
@@ -120,12 +131,28 @@ func GetLogs(c *gin.Context) {
 	}
 
 	logList := strings.Split(page, "\n")
-
 	logs := *new(Logs)
 
 	for _, line := range logList {
 		if strings.HasPrefix(line, "CRIT") || strings.HasPrefix(line, "ERROR") || strings.HasPrefix(line, "WARN") {
-			logDt := line[6:11] + "T" + line[12:24] + "Z"
+			logLevel := strings.Trim(line[0:5], " ")      //Log Level
+			logDt := line[6:11] + "T" + line[12:24] + "Z" //Log datetime
+			message := line[26:]                          //Log Description
+
+			//로그 제외 목록 체크
+			isExclude := false
+			for _, exclude := range EXCLUDE_LIST {
+				if strings.Contains(line, exclude) {
+					isExclude = true
+					break
+				}
+			}
+
+			if isExclude {
+				continue
+			}
+
+			//로그 날짜 형식 변환
 			month, _ := strconv.ParseInt(line[6:8], 10, 64)
 
 			if month > currentMonth {
@@ -136,14 +163,17 @@ func GetLogs(c *gin.Context) {
 
 			t1, _ := time.Parse(LAYOUT, logDt)
 
+			//범위 조건 검사 및 반환 형식화
 			if !hasRange || t1.After(startTime) {
 				log := &Log{
-					Level:       strings.Trim(line[0:5], " "),
+					Level:       logLevel,
 					DateTime:    t1.Unix(),
-					Description: line[26:],
+					Description: message,
 				}
 
 				logs = append(logs, *log)
+			} else {
+				break
 			}
 		}
 	}
